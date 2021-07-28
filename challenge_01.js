dbf0918bf0c874fa75cd33429a5e421b71fc8223
CC-Nobody <cc.adiermann@gmail.com>
2021-07-28 15:39:27 +0200

    Fancy Working Version 1.2

--- challenge_01.js
+++ challenge_01.js
@@ -9,14 +9,14 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
-// Hier werden die Variablen Definiert
+// Hier werden die Variablen definiert.
 let eingabe1;
 let eingabe2;
 let zahl1;
 let zahl2;
 let summe;
 
-// Dieser Abschnitt erstellt die Eingabeprompts
+// In diesem Abschnitt sind die Eingabeprompts.
 eingabe1 = prompt("Bitte eine Zahl eingeben:");
 eingabe2 = prompt("Bitte eine weitere Zahl eingeben:");
 
@@ -26,7 +26,7 @@ zahl2 = parseInt(eingabe2);
 summe = zahl1+zahl2;
 
 
-/* Extra logs zur Kontrolle & Problemlösung
+/* Extra logs zur Kontrolle & Problemlösung.
 console.log(typeof zahl1);
 console.log("Erster Wert:",zahl1);
 console.log(typeof zahl2);
@@ -34,7 +34,7 @@ console.log("Zweiter Wert:",zahl2);
 console.log ("Der Datentyp von Summe ist:",typeof summe);
 */
 
-// Ausgabe der Summe in einem if-else-Befehl
+// Ausgabe der Summe in einem if-else-Befehl um die Ausgabe "Die Summe der Zahlen ist: NaN" zu vermeiden.
 if (isNaN(summe)) {
     console.log("Eine Ihrer Eingaben war keine Zahl bitte versuchen Sie es erneut!")
 } else {
