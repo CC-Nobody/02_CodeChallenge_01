a5e4caea2d2f22e2d4dc81b4eba1c5c108b78a4a
CC-Nobody <cc.adiermann@gmail.com>
2021-07-28 14:16:06 +0200

    Draft1

--- challenge_01.js
+++ challenge_01.js
@@ -8,3 +8,19 @@ Berechnen Sie die Summe der Zahlen.
 Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
+
+// Var. frei definiert
+var zahl1;
+var zahl2;
+
+let summe;
+
+// Eingabe Prompts
+zahl1 = prompt("Bitte eine Zahl eingeben:");
+zahl2 = prompt("Bitte eine weitere Zahl eingeben:");
+
+//Rechnung
+summe = zahl1 + zahl2;
+
+//Ausgabe
+console.log ("Die Summer der Zahlen ist: summe");
